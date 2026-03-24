60c4d6b106ab65f58b1e108036c9744453b4191c
Master M <girangati1001@gmail.com>
2026-03-24 12:39:14 +0700

    RESTORE: Full Modules Recovery jam 7 pagi

---
+++ modules/modules.js
@@ -0,0 +1,6 @@
+// Dream OS v2.1 - Core Module Loader
+export const modules = [
+  'asset', 'booking', 'command-center', 'home', 
+  'janitor-in', 'janitor-out', 'k3', 'maintenance', 
+  'sekuriti', 'stok', 'profile', 'qr'
+];
