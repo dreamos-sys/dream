0119ac79dbb7d22a2a29a7d0eeaca570fc8b2206
Dream <afumum234@gmail.com>
2026-03-27 16:35:41 +0700

    Delete modules/modules.js

--- modules/modules.js
+++
@@ -1,6 +0,0 @@
-// Dream OS v2.1 - Core Module Loader
-export const modules = [
-  'asset', 'booking', 'command-center', 'home', 
-  'janitor-in', 'janitor-out', 'k3', 'maintenance', 
-  'sekuriti', 'stok', 'profile', 'qr'
-];
