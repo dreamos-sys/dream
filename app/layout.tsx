7a0e15d2ea2ecc04deddba23c72fdf2a2af27901
Master M <girangati1001@gmail.com>
2026-03-31 00:44:21 +0700

    Bismillah: Final Sovereign v2.1 - Killing Every Single Build Ghost

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,4 @@
 import './globals.css';
-export const metadata = { title: 'Dream OS v2.1', description: 'Sovereign Edition' };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="id">
-      <body>{children}</body>
-    </html>
-  );
+  return (<html lang="id"><body>{children}</body></html>);
 }
